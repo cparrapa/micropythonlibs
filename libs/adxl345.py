ff76116fbf15d11c2c1807683b062be450563e76
Alex <64538329+UEA-envsoft@users.noreply.github.com>
2024-06-28 16:27:52 +0100

    Updated to match new defined conventions

--- libs/adxl345.py
+++ libs/adxl345.py
@@ -1,8 +1,16 @@
+'''
+adxl345.py v01 28/06/24
+a library for the Analog Devices ADXL345 Digital Accelerometer
+Datasheet:
+https://www.analog.com/media/en/technical-documentation/data-sheets/adxl345.pdf
+based on information from
+https://howtomechatronics.com/tutorials/arduino/how-to-track-orientation-with-arduino-and-adxl345-accelerometer/
+v00, v01  Alex Etchells
+'''
+
 from machine import Pin, SoftI2C
 import math
 from time import sleep
-
-#See https://howtomechatronics.com/tutorials/arduino/how-to-track-orientation-with-arduino-and-adxl345-accelerometer/
 
 # ADXL-345 Registers 
 _DATA_FORMAT = const(0x31)
@@ -17,25 +25,28 @@ class ADXL345:
         self.i2c = i2c
         self.address = address
         self.i2c.start()
-        self.i2c.writeto(self.address, bytearray([_POWER_CTL, 0x08])) # power on
-        self.i2c.writeto(self.address, bytearray([_DATA_FORMAT, 0x08])) # full resolution mode
+        # power on
+        self.i2c.writeto(self.address, bytearray([_POWER_CTL, 0x08])) 
+        # -2g to +2g resolution
+        self.i2c.writeto(self.address, bytearray([_DATA_FORMAT, 0x08])) 
         self.i2c.stop()
-        self.calcerror()
+        self.Calibrate()
         
-    def readxyz(self):   #-2g to +2g is -512 to + 511
-        x, y, z = self.readrawxyz()
+    def ReadXYZ(self):   #-2g to +2g is -512 to + 511
+        x, y, z = self.ReadRawXYZ()
         return (x/256, y/256, z/256)
     
-    def readrawxyz(self):
+    def ReadRawXYZ(self):
         self.i2c.start()
         self.i2c.writeto(self.address, bytearray([_DATAX0]))
+        #read 6 registers, 2 for each co-ordinate
         data = self.i2c.readfrom(self.address, 6)
         self.i2c.stop()
         
         x = ((data[1] << 8) | data[0])
         x = x & 1023    #10 bit - was getting some noise in bits 11 - 16
         if x > 511:     #values are signed
-            x = x-1024        
+            x = x - 1024        
         y = ((data[3] << 8) | data[2])   
         y = y & 1023
         if y > 511:
@@ -43,29 +54,23 @@ class ADXL345:
         z = ((data[5] << 8) | data[4])
         z = z & 1023
         if z > 511:
-            z =z -1024
+            z = z -1024
         return (x, y, z)
           
-    def rollpitch(self):
-        x, y, z = self.readxyz()
-        #roll = (math.atan2(y , z) * 57.3 )#  (180/pi = ~57.3)
-        #pitch = (math.atan2((- x) , math.sqrt(y * y + z * z)) * 57.3)
-        
-        #still not happy with these calculations
-        
-        roll2= math.atan2(x , math.sqrt(pow(x, 2) + pow(z, 2))) * 180 / math.pi;
-        pitch = math.atan2(-x , math.sqrt(pow(y, 2) + pow(z, 2))) * 180 / math.pi;
-        return roll,pitch
-   
-   
+    #still not happy with these calculations
+    def RollPitch(self):
+        x,y,z = self.ReadXYZ()
+        roll = math.atan2(x , math.sqrt(pow(x, 2) + pow(z, 2))) * 180 / math.pi;
+        pitch = math.atan2(-x , math.sqrt(pow(y, 2) + pow(z, 2))) * 180 / math.pi;        
+        return (roll,pitch)
     
-    def calcerror(self):
+    def Calibrate(self):
         errX = 0
         errY = 0
         errZ = 0
-        
+        # take 200 readings
         for _ in range (200):
-            x, y, z = self.readrawxyz()
+            x, y, z = self.ReadRawXYZ()
             # Sum all readings            
             errX += x
             errY += y
@@ -97,5 +102,4 @@ class ADXL345:
         sleep(0.1)
         
                 
-
 
