20712f734b4a8e75a3189efde4d7bc86d0ee068d
Alex <64538329+UEA-envsoft@users.noreply.github.com>
2024-06-28 16:33:33 +0100

    updated to match changes in adxl345/py

--- accelerometer/adxl345_demo.py
+++ accelerometer/adxl345_demo.py
@@ -1,6 +1,6 @@
-import time
 from machine import Pin, SoftI2C
 from adxl345 import ADXL345
+import time
 
 # Define I2C pins
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
@@ -10,8 +10,8 @@ accel = ADXL345(i2c)
 
 # Read accelerometer data
 while True:
-    x, y, z = accel.readxyz()
+    x, y, z = accel.ReadXYZ()
     print("x = {}, y = {}, z = {}".format(x, y, z))
-    roll,pitch = accel.rollpitch()
-    print("roll = {0:.2f}, pitch = {0:.2f}".format(roll,pitch))    
+    roll,pitch = accel.RollPitch()
+    print("roll = {0:.2f}, pitch = {1:.2f}".format(roll,pitch))    
     time.sleep(0.5)
