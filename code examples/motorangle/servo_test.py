b39ee6f46146f3e20612264efcdea20540679c8e
cparrapa <cparrapa@gmail.com>
2025-05-16 09:05:27 +0200

    servo fixed

--- code examples/motorangle/servo_test.py
+++ code examples/motorangle/servo_test.py
@@ -7,18 +7,24 @@ adc_32 = ADC(32)
 adc_32.width(ADC.WIDTH_10BIT)
 
 servo_26 = Servo()
-servo_26.attach(26)
+servo_26.attach(15)
 
 servo_26.write(90)
 time.sleep(1)
-servo_26.write(135)
+servo_26.write(120)
+time.sleep(1)
+servo_26.write(160)
 time.sleep(1)
 servo_26.write(180)
 time.sleep(1)
 servo_26.write(90)
 time.sleep(1)
+servo_26.write(60)
+time.sleep(1)
+servo_26.write(30)
+time.sleep(1)
 servo_26.write(0)
-
+time.sleep(1)
 def map(value, in_min, in_max, out_min, out_max):
    map = (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
    return map
---
+++ code examples/ultrasonic/ultrasonic_fade.py
@@ -0,0 +1,58 @@
+from time import sleep
+from machine import Pin
+from neopixel import NeoPixel
+
+n = 13 # Number of LEDs in the ring
+ring = NeoPixel(Pin(4), n) # Connector 5
+p = 6  # Number of LEDs in the ultrasonic RGB
+ultra = NeoPixel(Pin(18), p) # Connector 1
+WheelPos = 0
+
+def Wheel():
+    global WheelPos, n, L, i
+    WheelPos = (255) - WheelPos
+    if WheelPos < (85):
+        for L in range(n):
+            ring[L] = (255 - WheelPos * 3, 0, WheelPos * 3 )
+        ring.write()
+
+
+    elif WheelPos < (170):
+        WheelPos -= 85
+        for L in range(n):
+            ring[L] = (0, WheelPos * 3, 255 - WheelPos * 3)
+        ring.write()
+
+    else:
+        WheelPos -= 170
+        for L in range(n):
+            ring[L] = (WheelPos * 3, 255 - WheelPos * 3, 0)
+        ring.write()
+
+def Wheelultra():
+    global WheelPos, p, L, i
+    WheelPos = (255) - WheelPos
+    if WheelPos < (85):
+        for L in range(p):
+            ultra[L] = (255 - WheelPos * 3, 0, WheelPos * 3 )
+        ultra.write()
+
+    elif WheelPos < (170):
+        WheelPos -= 85
+        for L in range(p):
+            ultra[L] = (0, WheelPos * 3, 255 - WheelPos * 3)
+        ultra.write()
+
+    else:
+        WheelPos -= 170
+        for L in range(p):
+            ultra[L] = (WheelPos * 3, 255 - WheelPos * 3, 0)
+        ultra.write()
+
+while True:
+    for i in range(255):
+        WheelPos = i
+        Wheel()
+        Wheelultra()
+        sleep(0.1)
+
