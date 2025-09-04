2086af2ecbbf13ee0df1335a392b712224721134
unknown <cparrapa@gmail.com>
2025-09-04 10:43:33 +0200

    espnow control UX improved

--- code examples/espnow/Controller_sender4buttons.py
+++ code examples/espnow/Controller_sender4buttons.py
@@ -11,18 +11,17 @@ sta.disconnect()
 esp = espnow.ESPNow()
 esp.active(True)
 
-# Define receiving ESP32 MAC address
-peer = b'\x08\xd1\xf9\xfb\xf5\xc8'
+# Define receiving Otto MAC address, use Mac Address finder.py code to find it
+peer = b'\xb0\xa72#\xc9h'
 esp.add_peer(peer)
 
 # Set up buttons, buzzer, and LED
-buttonA_pin = Pin(26, Pin.IN, Pin.PULL_UP) # Connector 4
-buttonB_pin = Pin(32, Pin.IN, Pin.PULL_UP) # Connector 6
-buttonC_pin = Pin(4, Pin.IN, Pin.PULL_UP)  # Connector 5
-buttonD_pin = Pin(33, Pin.IN, Pin.PULL_UP) # Connector 7
-buttonE_pin = Pin(15, Pin.IN, Pin.PULL_UP)
-buttonF_pin = Pin(13, Pin.IN, Pin.PULL_UP)
-
+buttonA_pin = Pin(26, Pin.IN, Pin.PULL_UP) # Connector 4 UP forward
+buttonB_pin = Pin(32, Pin.IN, Pin.PULL_UP) # Connector 6 DOWN backward
+buttonC_pin = Pin(4 , Pin.IN, Pin.PULL_UP) # Connector 5 LEFT turn
+buttonD_pin = Pin(33, Pin.IN, Pin.PULL_UP) # Connector 7 RIGTH turn
+buttonE_pin = Pin(15, Pin.IN, Pin.PULL_UP) # Connector 9
+buttonF_pin = Pin(13, Pin.IN, Pin.PULL_UP) # Connector 11
 buzzer = OttoBuzzer(25)
 led = Pin(2, Pin.OUT)
 
@@ -42,8 +41,8 @@ while True:
     if current_buttonA_state != last_buttonA_state:
         utime.sleep_ms(debounce_delay)
         current_buttonA_state = buttonA_pin.value()
-        if current_buttonA_state == 1:
-            message = "AOn"
+        if current_buttonA_state == 1: # CHANGE TO 0 if button is Normally Open
+            message = "A" # Press UP
         else:
             message = "AOff"
         print(f"Sending command: {message}")
@@ -56,7 +55,7 @@ while True:
         utime.sleep_ms(debounce_delay)
         current_buttonB_state = buttonB_pin.value()
         if current_buttonB_state == 1:
-            message = "Bon"
+            message = "B" # Press DOWN
             led.on()
         else:
             message = "Boff"
@@ -71,7 +70,7 @@ while True:
         utime.sleep_ms(debounce_delay)
         current_buttonC_state = buttonC_pin.value()
         if current_buttonC_state == 1:
-            message = "Con"
+            message = "C" # Press LEFT
         else:
             message = "Coff"
         print(f"Sending command: {message}")
@@ -84,7 +83,7 @@ while True:
         utime.sleep_ms(debounce_delay)
         current_buttonD_state = buttonD_pin.value()
         if current_buttonD_state == 1:
-            message = "Don"
+            message = "D" # Press RIGHT
         else:
             message = "Doff"
         print(f"Sending command: {message}")
@@ -92,37 +91,49 @@ while True:
         last_buttonD_state = current_buttonD_state
         
     # Check for button combinations
-    if buttonA_pin.value() == 1 and buttonC_pin.value() == 0:
-        message = "Eon"
+    if buttonA_pin.value() == 1 and buttonC_pin.value() == 1:
+        message = "AC"
         print(f"Sending combo command: {message}")
         esp.send(peer, message.encode())
         utime.sleep_ms(300)  # Prevent multiple triggers
 
-    elif buttonA_pin.value() == 1 and buttonD_pin.value() == 0:
-        message = "Fon"
+    elif buttonA_pin.value() == 1 and buttonD_pin.value() == 1:
+        message = "AD"
         print(f"Sending combo command: {message}")
         esp.send(peer, message.encode())
         utime.sleep_ms(300)
 
-    elif buttonB_pin.value() == 1 and buttonC_pin.value() == 0:
-        message = "Gon"
+    elif buttonB_pin.value() == 1 and buttonC_pin.value() == 1:
+        message = "BC"
         print(f"Sending combo command: {message}")
         esp.send(peer, message.encode())
         utime.sleep_ms(300)
 
-    elif buttonB_pin.value() == 1 and buttonD_pin.value() == 0:
-        message = "Hon"
+    elif buttonB_pin.value() == 1 and buttonD_pin.value() == 1:
+        message = "BD"
         print(f"Sending combo command: {message}")
         esp.send(peer, message.encode())
         utime.sleep_ms(300)
-
+        
+    elif buttonA_pin.value() == 1 and buttonB_pin.value() == 1:
+        message = "AB"
+        print(f"Sending combo command: {message}")
+        esp.send(peer, message.encode())
+        utime.sleep_ms(300)
+        
+    elif buttonC_pin.value() == 1 and buttonD_pin.value() == 1:
+        message = "CD"
+        print(f"Sending combo command: {message}")
+        esp.send(peer, message.encode())
+        utime.sleep_ms(300)
+"""
     # Debounce Button E
     current_buttonE_state = buttonE_pin.value()
     if current_buttonE_state != last_buttonE_state:
         utime.sleep_ms(debounce_delay)
         current_buttonE_state = buttonE_pin.value()
-        if current_buttonE_state == 0:
-            message = "Eon"
+        if current_buttonE_state == 1:
+            message = "E"
         else:
             message = "Eoff"
         print(f"Sending command: {message}")
@@ -134,10 +145,11 @@ while True:
     if current_buttonF_state != last_buttonF_state:
         utime.sleep_ms(debounce_delay)
         current_buttonF_state = buttonF_pin.value()
-        if current_buttonF_state == 0:
-            message = "Fon"
+        if current_buttonF_state == 1:
+            message = "F"
         else:
             message = "Foff"
         print(f"Sending command: {message}")
         esp.send(peer, message.encode())
         last_buttonF_state = current_buttonF_state
+"""
--- code examples/espnow/Starter_receiver4buttons.py
+++ code examples/espnow/Starter_receiver4buttons.py
@@ -1,10 +1,10 @@
 from machine import Pin, ADC, PWM, SoftI2C
-from neopixel import NeoPixel
 from time import sleep
+from neopixel import NeoPixel
 from ottoneopixel import OttoNeoPixel
+from ottoneopixel import OttoUltrasonic
 from ottobuzzer import OttoBuzzer
 from ottomotor import OttoMotor
-from ottoneopixel import OttoUltrasonic
 import network, espnow, machine
 
 offset = 0
@@ -18,23 +18,44 @@ sta.disconnect()
 esp = espnow.ESPNow()
 esp.active(True)
 
-led_pin = machine.Pin(2, machine.Pin.OUT)
+led_pin = Pin(2, Pin.OUT)
 buzzer = OttoBuzzer(25)
 motor = OttoMotor(13, 14)             # Connectors 10 & 11
 ultrasonic = OttoUltrasonic(18, 19)   # Connector 1
 motor.leftServo.freq(50)
 motor.rightServo.freq(50)
 ring = OttoNeoPixel(4, 13)             # Connector 5
-ring.fillAllRing(0, 255, 255)
-ultrasonic.ultrasonicRGB2(0, 255, 255)
+ring.setRGBring(0, "fe0000")
+ultrasonic.ultrasonicRGB2(255, 0, 0)
+buzzer.playNote(261, 75)
+ring.setRGBring(1, "fe0000")
+ring.setRGBring(2, "fe0000")
+buzzer.playNote(293, 75)
+ring.setRGBring(3, "fe0000")
+ring.setRGBring(4, "fe0000")
+buzzer.playNote(329, 75)
+ring.setRGBring(5, "fe0000")
+ring.setRGBring(6, "fe0000")
+buzzer.playNote(349, 75)
+ring.setRGBring(7, "fe0000")
+ring.setRGBring(8, "fe0000")
+buzzer.playNote(392, 75)
+ring.setRGBring(9, "fe0000")
+buzzer.playNote(440, 75)
+ring.setRGBring(10, "fe0000")
+buzzer.playNote(493, 75)
+ring.setRGBring(11, "fe0000")
+buzzer.playNote(523, 75)
+ring.setRGBring(12, "fe0000")
 
 while True:
     _, msg = esp.recv()
     if msg:             # msg == None if timeout in recv()
-        if msg == b'AOn':
+        if msg == b'A': # UP pressed forward
             print("Turning on LED")
             led_pin.on()
-            ring.fillAllRing(255, 0, 0)
+                            # 0			1		2			3		4			5			6		7			8		9			10		11			12
+            ring.fillRGBRing("fe0000", "fe0000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "fe0000", "fe0000")
             buzzer.playNote(261, 125)
             motor.leftServo.duty(127- offset)
             motor.rightServo.duty(29+ offset)
@@ -42,51 +63,65 @@ while True:
             print("Turning off LED")
             led_pin.off()
             ring.fillAllRing(0, 0, 0)
+            ring.setRGBring(0, "fe0000")
             motor.rightServo.duty(0)
             motor.leftServo.duty(0)
-        elif msg == b'Bon':
-            ring.fillAllRing(255, 130, 0)
+        elif msg == b'B': # DOWN oressed backward
+            led_pin.on()
+            ring.fillRGBRing("fe0000", "000000", "000000", "000000", "000000", "fe0000", "fe0000", "fe0000", "000000", "000000", "000000", "000000", "000000")
             buzzer.playNote(293, 125)
             motor.leftServo.duty(43- offset)
             motor.rightServo.duty(109+ offset)
             sleep(0.1)
         elif msg == b'Boff':
+            led_pin.off()
             ring.fillAllRing(0, 0, 0)
+            ring.setRGBring(0, "fe0000")
             motor.rightServo.duty(0)
             motor.leftServo.duty(0)
-        elif msg == b'Con':
-            ring.fillAllRing(255, 255, 0)
+        elif msg == b'C': # Turn LEFT pressed
+            led_pin.on()
+            ring.fillRGBRing("fe0000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "fe0000", "fe0000", "fe0000", "000000", "000000")
             buzzer.playNote(329, 125)
             motor.rightServo.duty(100+ offset)
             motor.leftServo.duty(100- offset)
         elif msg == b'Coff':
+            led_pin.off()
             ring.fillAllRing(0, 0, 0)
+            ring.setRGBring(0, "fe0000")
             motor.rightServo.duty(0)
             motor.leftServo.duty(0)
-        elif msg == b'Don':
-            ring.fillAllRing(0, 255, 0)
+        elif msg == b'D': # Turn RIGHT pressed
+            led_pin.on()
+            ring.fillRGBRing("fe0000", "000000", "fe0000", "fe0000", "fe0000", "000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000")
             buzzer.playNote(349, 125)
             motor.rightServo.duty(70+ offset)
             motor.leftServo.duty(70- offset)
         elif msg == b'Doff':
+            led_pin.off()
             ring.fillAllRing(0, 0, 0)
+            ring.setRGBring(0, "fe0000")
             motor.rightServo.duty(0)
             motor.leftServo.duty(0)
-        elif msg == b'Eon':
+            """
+        elif msg == b'E':
             ring.fillAllRing(0, 255, 130)
             buzzer.playNote(392, 125)
             ultrasonic.ultrasonicRGB2(0, 255, 130)
             motor.rightServo.duty(60+ offset)
             motor.leftServo.duty(60- offset)
         elif msg == b'Eoff':
+            led_pin.off()
             ring.fillAllRing(0, 0, 0)
-        elif msg == b'Fon':
+        elif msg == b'F':
             ring.fillAllRing(0, 120, 255)
             ultrasonic.ultrasonicRGB2(0, 120, 255)
             buzzer.playNote(440, 125)
             motor.rightServo.duty(95+ offset)
             motor.leftServo.duty(95- offset)
         elif msg == b'Foff':
+            led_pin.off()
             ring.fillAllRing(0, 0, 0)
+            """
         else:
             print("Unknown message!")
